fe274ea02b1225265b8bb9a58d97e4e62a409b3c
sj-ucsd <78191747+sj-ucsd@users.noreply.github.com>
2023-10-22 09:10:11 -0700

    updated files to process mapping configuration file correctly

--- sstopgraph_pkg/setup.py
+++ sstopgraph_pkg/setup.py
@@ -5,6 +5,5 @@ setup(
     version='1.1',
     packages=find_packages(),
     install_requires=[
-    "jsonpath_ng"
     ],
 )
--- sstopgraph_pkg/sstopgraph/sstopgraph.py
+++ sstopgraph_pkg/sstopgraph/sstopgraph.py
@@ -1,42 +1,89 @@
 import json
-def semistruct_to_pgraph(in_file, mapcfg_file, out_file='./pgraph.txt',debug=False):
-   # check for input errors
-   if not in_file:
-     print('Error! input file missing')
-     sys.exit(2)
-   elif not mapcfg_file:
-     print('Error! mapping configuration file missing')
-     sys.exit(2)
+import sys
+
+def _load_json_data(input_file):
+   try:
+      with open(input_file,'r') as input_json:
+         json_data = json.load(input_json)
+      return json_data
+   except Exception as e:
+      print("Input file Error! {}".format(e) )
+      return sys.exit(2)
+
+
+def semistruct_to_pgraph(in_file, mapcfg_file, out_file='./pgraph.json',debug=False):
+
    if debug:
-     print('input file = {}, mapping configuration file = {}, output file = {}'.format(in_file, mapcfg_file, out_file))
+      print('input file = {}, mapping configuration file = {}, output file = {}'.format(in_file, mapcfg_file, out_file))
 
    # read file input file containing json format semi-structured data 
    # and mapping configuraton file in json format
-   try:
-      with open(in_file,'r') as input_json:
-        semi_struct = json.load(input_json)
-      if debug:
-        print(semi_struct)
-   except Exception as e:
-      print("Input file Error! {}".format(e) )
-   
-   try:
-      with open(mapcfg_file, "r") as mapping_file:
-        mapping_config = json.load(mapping_file)
-      if debug:
-        print(mapping_config)
-   except Exception as e:
-      print("Mapping configuration file Error! {}".format(e) )
-   
+      
+   source_data = _load_json_data(in_file)
+   mapping_config = _load_json_data(mapcfg_file)
+   if debug:
+      print("input data:")
+      print(source_data)
+      print("mapping configuration :")
+      print(mapping_config)
+
    # Initialize the property graph
    property_graph = {
-     "graph": {
-        "nodes": [],
-        "edges": []
-     }
+      "graph": {
+         "nodes": [],
+         "edges": []
+      }
    }
 
+   # add nodes to the property graph
+   for element in source_data[mapping_config['schema-map']['nodes'][0]['label']]:
+      node = {
+         "node": {
+            "id": element[mapping_config['schema-map']['nodes'][0]['node-id']],
+            "label": mapping_config['schema-map']['nodes'][0]['label'],
+            "properties":{} #mapping_config['schema-map']['nodes'][0]['node-properties']
+         }
+      }
+
+      # Map properties to the node
+      for property_name, property_mapping in mapping_config['schema-map']['nodes'][0]['node-properties'].items():
+         property_value = element
+         for key in property_mapping.split('.'):
+            property_value = property_value.get(key, None)
+            if property_value is None:
+               break
+
+         if property_value is not None:
+            node["node"]["properties"][property_name] = {
+            "datatype": "string",  # comment : modify this based on the actual data types
+            "data value": property_value
+            }
+
+      property_graph["graph"]["nodes"].append(node)
+    
+
+   # source data and add edges to the property graph
+   for element in source_data[mapping_config['schema-map']['nodes'][0]['label']]:
+      source_id = element[mapping_config['schema-map']['nodes'][0]['node-id']]
+      target = element.get(mapping_config['schema-map']['edges'][0]['edge-type'], [])
+
+      for target_id in target:
+         edge = {
+            "edge": {
+               "id": len(property_graph["graph"]["edges"]) + 1,
+               "relationship": mapping_config['schema-map']['edges'][0]['relationship'],
+               "from_node_id": source_id,
+               "to_node_id": target_id,
+               "properties": {}
+            }
+         }
+
+         property_graph["graph"]["edges"].append(edge)
+
    # Save the property graph in JSON format
+   if debug:
+     print("generated property graph:")
+     print(property_graph)
    with open(out_file, "w") as output_file:
      json.dump(property_graph, output_file, indent=4)
 
