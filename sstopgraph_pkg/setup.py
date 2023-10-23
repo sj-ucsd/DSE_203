d4bb7de583fa8758cdf7ea37dc27afd0b3aacade
sj-ucsd <78191747+sj-ucsd@users.noreply.github.com>
2023-10-22 17:50:52 -0700

    updated setup file to include author names

--- sstopgraph_pkg/setup.py
+++ sstopgraph_pkg/setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='sstopgraph-pkg',
     version='1.1',
+    author='Sagar Jogadhenu, Laben Fisher, Prakhar Shukla',
+    description='Package to convert semistructred \
+                 (json) data to property graph',
     packages=find_packages(),
     install_requires=[
+    "jsonpath_ng"
     ],
 )
