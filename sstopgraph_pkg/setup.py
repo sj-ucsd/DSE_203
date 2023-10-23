6cd85997a3315c03df98322961904de42fb1a47a
sj-ucsd <78191747+sj-ucsd@users.noreply.github.com>
2023-10-22 17:51:35 -0700

    updated version number

--- sstopgraph_pkg/setup.py
+++ sstopgraph_pkg/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sstopgraph-pkg',
-    version='1.1',
+    version='1.1.2',
     author='Sagar Jogadhenu, Laben Fisher, Prakhar Shukla',
     description='Package to convert semistructred \
                  (json) data to property graph',
